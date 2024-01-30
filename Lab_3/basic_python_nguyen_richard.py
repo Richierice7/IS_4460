56b5ae3dd58a9680f0478d8270552dac4b8e854d
Richierice7 <rnguyen9530@gmail.com>
2024-01-30 10:13:42 -0700

    1/30

---
+++ Lab_3/basic_python_nguyen_richard.py
@@ -0,0 +1,35 @@
+#My first comment!
+
+print("Darth Vader") #this is an object
+print(4.0) #this is a float
+
+d = 6
+D = 16
+print(d)
+print(D)
+
+character_name = "Darth Vader"
+character_gpa = 1.4
+print(character_name)
+print(character_gpa)
+
+print("My name is " + character_name + " not Luke")
+
+print(f"My name is {character_name} not Luke")
+
+num  = 43213412421/34
+print(str(num)[1:5])
+def multiply_num(a,b):
+  out = a*b
+  return out
+
+print(multiply_num(4,5))
+print(multiply_num(b = 32,a = 21))
+
+name = "Bobby"
+
+def hello_world():
+  name = "Eddy"
+  print(name)
+
+hello_world()
---
+++ Lab_3/python_expressions_control_nguyen_richard.py
@@ -0,0 +1,6 @@
+print(f"a: {20 > 12}")
+print(f"b: {20 < 12}")
+print(f"c: {20 == 12}")
+
+print(f"Is 15 less than 12?", int(15<12))
+print(f"Is 20 equal to 20?", int(20 == 20))
