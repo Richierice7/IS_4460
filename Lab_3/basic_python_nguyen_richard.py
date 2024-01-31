e3a2bd37683bdef65e9db4e212c1bb0a6d2175bd
Richierice7 <rnguyen9530@gmail.com>
2024-01-31 13:14:19 -0700

    1/31

--- Lab_3/basic_python_nguyen_richard.py
+++ Lab_3/basic_python_nguyen_richard.py
@@ -1,13 +1,17 @@
+#Part 1: Comments
 #My first comment!
 
+#Part 2: Literals
 print("Darth Vader") #this is an object
 print(4.0) #this is a float
 
+#Part 3: Variable Names
 d = 6
 D = 16
 print(d)
 print(D)
 
+#Part 4: Strings and Variables
 character_name = "Darth Vader"
 character_gpa = 1.4
 print(character_name)
@@ -17,15 +21,19 @@ print("My name is " + character_name + " not Luke")
 
 print(f"My name is {character_name} not Luke")
 
-num  = 43213412421/34
+#Part 5: Converting between types
+num  = 432134/34
 print(str(num)[1:5])
+
+#Part 6: a,b function
 def multiply_num(a,b):
-  out = a*b
-  return out
+  output = a*b
+  return output
 
 print(multiply_num(4,5))
 print(multiply_num(b = 32,a = 21))
 
+#Part 7: say_hello function
 name = "Bobby"
 
 def hello_world():
--- Lab_3/python_expressions_control_nguyen_richard.py
+++ Lab_3/python_expressions_control_nguyen_richard.py
@@ -1,6 +1,97 @@
+#Part 8: boolean
 print(f"a: {20 > 12}")
 print(f"b: {20 < 12}")
 print(f"c: {20 == 12}")
 
+#Part 9: boolean is int
 print(f"Is 15 less than 12?", int(15<12))
 print(f"Is 20 equal to 20?", int(20 == 20))
+
+#Part 10: Literals and Variables
+my_name = "richard"
+my_age = 22
+print(f"a: {17}")
+print(f"b: {'Hi'}")
+print(f"c: {True}")
+print(f"d: {my_name}")
+print(f"e: {my_age}")
+
+#Part 11: Precedence
+print((4+10/2),(4+1*6))
+print((4*10/2),(4/1-6))
+print((4/10-2),(4-1+6))
+
+#Part 12: Relational Operators
+print(f"Is 'rich' == 'richard'? {'rich' == 'richard'}")
+
+#Part 13: Equality Operators
+my_name = "rich"
+print("assignment: ", my_name)
+print("equality: ", my_name == "rich")
+
+#Part 14: Comparison Operators
+print("comparison:", "aa" > "a")
+print("comparison:", 1 < 2)
+
+a = 7
+b = 13
+print(f"Comparision: {a} is greater than {b}" if a > b else "")
+print(f"Comparision: {a} is less than {b}" if a < b else "")
+print(f"Comparision: {a} is greater than or equal to {b}" if a >= b else "")
+print(f"Comparision: {a} is less than or equal to {b}" if a <= b else "")
+
+#Part 15: If Statements
+my_bank_balance = 0
+if my_bank_balance < 100:
+    money = 150
+    my_bank_balance += money
+
+#Part 16: Else Statements
+my_bank_balance = 0
+if my_bank_balance > 100:
+    money = 150
+    my_bank_balance += money
+else:
+    print("I'm poor")
+
+#Part 17: Elif Statements
+checking = 200
+savings = 3500
+if checking < 1000:
+    money = 500
+    checking += money
+elif checking > 100:
+    savings += 200
+    checking -= 200
+else:
+    savings += 100
+    checking -= 100
+print(checking)
+print(savings)
+
+#Part 18: Ternary Statements
+fuel = 7
+print("Fill tank now" if fuel <= 10 else "There's not enough fuel")
+
+#While Loops
+fuel = 10
+while fuel > 1:
+    print("There's not enough fuel")
+    fuel -=1
+
+#For Loops
+movies = ["barbie", "oppenheimer", "tmnt"]
+for movie in movies:
+    print(f"movie: {movie}")
+for i in range (5):
+    print(f'i: {i}')
+
+for count in range(10):
+    print(f"{count} times 7 is {count * 7}")
+    if count == 5:
+        break
+
+for count in range(10):
+    if count == 5:
+        continue
+    print(f"{count} times 7 is {count * 7}")
