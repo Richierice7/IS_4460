cc4d0adbe471a138562c233f93b9f01bcfcef314
Richierice7 <rnguyen9530@gmail.com>
2024-02-07 10:39:44 -0700

    2/7

--- lab4/my_proj/my_app/views.py
+++ lab4/my_proj/my_app/views.py
@@ -1,7 +1,14 @@
 from django.views import View
 from django.shortcuts import render, redirect
 
+def title_name(the_name: str):
+    return the_name.title()
+
 class HomePageView(View):
     def get(self, request):
      
-        return render(request, 'my_app/index.html',{'hi':'hello world!'})
+        my_name = "RICHARD"
+        new_name = title_name(my_name)
+        the_context = {'hi':'hello CLASS!', 'name':new_name}
+
+        return render(request, 'my_app/index.html', the_context)
