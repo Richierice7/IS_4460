ffbd800ddf5fe30086d7b69cf6e2600d5764f9e0
Richierice7 <rnguyen9530@gmail.com>
2024-02-07 13:39:46 -0700

    2/7

---
+++ lab4/my_proj/my_app/my_objects.py
@@ -0,0 +1,13 @@
+
+class vehicle():
+    def __init__(self):
+        self.type = 'generic vehicle'
+
+
+
+        
+class car(vehicle):
+    def __init__(self,color,sound):
+        self.color = color
+        self.sound = sound
+        self.wheels = 4
--- lab4/my_proj/my_app/views.py
+++ lab4/my_proj/my_app/views.py
@@ -1,14 +1,39 @@
 from django.views import View
 from django.shortcuts import render, redirect
+from .import my_functions, my_objects
 
 def title_name(the_name: str):
+    
     return the_name.title()
 
+
+
 class HomePageView(View):
+
     def get(self, request):
      
         my_name = "RICHARD"
+
         new_name = title_name(my_name)
-        the_context = {'hi':'hello CLASS!', 'name':new_name}
+
+        the_names = ['Rich', 'Richie', 'Richard']
+
+        other_names = the_names
+        the_names.append('Tim')
+
+        new_names = my_functions.title_names(the_names)
+
+        car1 = my_objects.car('green', 'honk honk')
+        car2 = my_objects.car('blue', 'beep beep')
+
+        the_context = {'hi':'hello CLASS!',
+                       'name':new_name,
+                       'names_list':other_names,
+                       'car1':car1,
+                       'car2':car2
+                       
+                       
+                       
+                       }
 
         return render(request, 'my_app/index.html', the_context)
